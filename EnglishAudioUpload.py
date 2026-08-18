1a70b77aabb55069f78e1ed96f1401a4cbd06f32
Drzumaya <dreducador@gmail.com>
2026-08-17 23:57:36 -0700

    Change Google Sheets ID and export URL
    Updated the spreadsheet ID and URL for Google Sheets export.

--- EnglishAudioUpload.py
+++ EnglishAudioUpload.py
@@ -18,8 +18,8 @@ if "student_code" not in st.session_state:
     st.session_state.student_code = None
 
 # Configuración del puente con el Google Sheet usando tu ID verificado
-SPREADSHEET_ID = "1vnRZDlb79scuC4kkdy0X3QNJKSLsVUFe_YoUe8GZlQU"
-URL_SHEET = f"https://google.com{SPREADSHEET_ID}/export?format=csv"
+SPREADSHEET_ID = "2PACX-1vR14gLuF0ogpRIDP_OGmAff4akh2JdUKLVawIgBVd4AJhK796f1-uonX-2aLVaIW2nFtzyGsWe0yCLP"
+URL_SHEET = f"https://docs.google.com/spreadsheets/d/e/2PACX-1vR14gLuF0ogpRIDP_OGmAff4akh2JdUKLVawIgBVd4AJhK796f1-uonX-2aLVaIW2nFtzyGsWe0yCLP/pub?output=csv"
 
 
 # ==============================================================================
